76c1c7eb7700da89ab27b81999735b6f55c3c333
Harish121292 <harishvijay12@gmail.com>
2025-12-24 23:15:17 +0530

    Added method2 and Git2 Class from Tester1

---
+++ src/org/git/Git2.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Git2 {
+
+	private void method3() {
+		System.out.println("Cucumber");
+	}
+
+	public static void main(String[] args) {
+		Git2 s = new Git2();
+		s.method3();
+	}
+
+}
