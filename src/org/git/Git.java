ff420d7c2684a5a554c2ae58ca56f2b698cca769
Harish121292 <harishvijay12@gmail.com>
2025-12-24 21:54:23 +0530

    First time push

---
+++ src/org/git/Git.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class Git {
+
+	private void method1() {
+		System.out.println("Selenium");
+	}
+
+	public static void main(String[] args) {
+		Git g = new Git();
+		g.method1();
+
+	}
+
+}
