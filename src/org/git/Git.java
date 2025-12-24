3c449a66ac9e7203b9c18ce0c314e54c7755db78
Harish121292 <harishvijay12@gmail.com>
2025-12-24 23:55:52 +0530

    Added method from Tester2

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -6,9 +6,14 @@ public class Git {
 		System.out.println("Selenium");
 	}
 
+	private void method4() {
+		System.out.println("Java");
+	}
+
 	public static void main(String[] args) {
 		Git g = new Git();
 		g.method1();
+		g.method4();
 
 	}
 
